09912f363054c1b29a9f6269ed458734b2cf35df
Morimx <82421851+Morimx@users.noreply.github.com>
2022-09-05 17:41:21 -0300

    No estaba tomando el commit

--- clase4/desafio-clase4.js
+++ clase4/desafio-clase4.js
@@ -8,10 +8,7 @@ class Contenedor {
   save(objeto) {
     const contenido = fs.readFileSync(this.archivo, "utf-8");
     const productos = JSON.parse(contenido);
-    const id =
-      productos.length > 0
-        ? productos[productos.length - 1].id + 1
-        : productos.length + 1;
+    const id = productos.length + 1;
     const producto = { id, ...objeto };
     productos.push(producto);
     fs.writeFileSync(this.archivo, JSON.stringify(productos, null, 2));
@@ -50,6 +47,21 @@ class Contenedor {
     const dataParseada = JSON.parse(data);
     let random = dataParseada[Math.floor(Math.random() * dataParseada.length)];
     return random;
+  }
+
+  updateById(id, objetoNuevo) {
+    const data = fs.readFileSync(this.archivo, "utf-8");
+    let dataParseada = JSON.parse(data);
+    let productoViejo = dataParseada.find((objeto) => objeto.id === id);
+    let mensaje = "Se reemplazo el producto";
+    if (productoViejo === undefined) {
+      throw { msg: "404 Not found" };
+    }
+    let productosFiltrados = dataParseada.filter((objeto) => objeto.id !== id);
+    productoViejo = { id, ...objetoNuevo };
+    productosFiltrados.push(productoViejo);
+    fs.writeFileSync(this.archivo, JSON.stringify(productosFiltrados, null, 2));
+    return mensaje;
   }
 }
 
