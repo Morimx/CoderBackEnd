4b23f6099adac06bd950aa5c5dd848ac67fad8dc
Morimx <82421851+Morimx@users.noreply.github.com>
2022-09-16 20:22:53 -0300

    usando partial layouts

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ const productosRouter = require("./productos");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use("/", express.static(__dirname + "/assets"));
-
 app.engine(
   "hbs",
   handlebars.engine({
@@ -33,54 +31,11 @@ app.get("/", (req, res) => {
     title: "Página principal",
     Precio: "Precio",
     addProd: "Añadir Producto",
-  });
-});
-
-app.get("/productos", (req, res) => {
-  res.render("productos", {
-    layout: "productos",
-    title: "Productos",
     compras: constructor.getAll().sort((a, b) => a.id - b.id),
     noProd: "No hay productos",
+    partialsPath: __dirname + "/views/partials",
   });
 });
-
-/////////////////////////
-// PUG VIEWS/////
-/////////////////////////
-// app.set("views", __dirname + "/views");
-// app.set("view engine", "pug");
-// app.get("/", (req, res) => {
-//   res.render("root", {
-//     layout: "root",
-//     title: "Página principal",
-//     Precio: "Precio",
-//     addProd: "Añadir Producto",
-//   });
-// });
-
-// app.get("/productos", (req, res) => {
-//   res.render("productos", {
-//     layout: "productos",
-//     title: "Productos",
-//     compras: constructor.getAll().sort((a, b) => a.id - b.id),
-//     noProd: "No hay productos",
-//   });
-// });
-
-/////////////////////////
-// EJS VIEWS/////
-/////////////////////////
-// app.set("views", __dirname + "/views");
-// app.set("view engine", "ejs");
-// app.get("/", (req, res) => {
-//   res.render("root", {
-//     layout: "root",
-//     title: "Página principal",
-//     Precio: "Precio",
-//     addProd: "Añadir Producto",
-//   });
-// });
 
 // app.get("/productos", (req, res) => {
 //   res.render("productos", {
